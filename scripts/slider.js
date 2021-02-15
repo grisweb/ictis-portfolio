78ee70cde0b23eaae235e342ab8be8612247d7d0
grisweb <grishakvlad@gmail.com>
2021-02-15 05:13:52 +0300

    15.02.2021

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,21 +1,16 @@
-// let slideIndex = 0;
-// let slides = document.getElementsByClassName("slider-news__slide");
-//
-// function showSlides(n) {
-//     console.log(slideIndex);
-//     slides[slideIndex].classList.remove("slider-news__slide--active");
-//     slideIndex += n;
-//
-//     if (slideIndex === slides.length) {
-//         slideIndex = 0;
-//     }
-//     if (slideIndex < 0) {
-//         slideIndex = slides.length - 1;
-//     }
-//     console.log(slideIndex);
-//     slides[slideIndex].className += " slider-news__slide--active";
-// }
-
 $(document).ready(function () {
-    $(".slider-news").slick();
+    $(".slider-news").slick({
+        prevArrow: '<button class="slide-button slide-button--prev">\n' +
+            '    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '        <path d="M1 1L5 5L9 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+            '    </svg>\n' +
+            '</button>',
+        nextArrow: '<button class="slide-button slide-button--next">\n' +
+            '    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '        <path d="M1 1L5 5L9 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+            '    </svg>\n' +
+            '</button>',
+        autoplay:true,
+        autoplaySpeed: 5000
+    });
 })
