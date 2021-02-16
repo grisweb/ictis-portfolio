ae7ac3c0bcff97ee74a5e09f979caf3b9bf57326
grisweb <grishakvlad@gmail.com>
2021-02-16 16:45:33 +0300

    16.02.2021

--- scripts/slider.js
+++ scripts/slider.js
@@ -13,4 +13,22 @@ $(document).ready(function () {
         autoplay:true,
         autoplaySpeed: 5000
     });
+});
+
+$(document).ready(function () {
+    $(".slider-mentors").slick({
+        slidesToShow: 3,
+        infinite: false,
+        //centerMode: true,
+        prevArrow: '<button class="slide-button slide-button--prev">\n' +
+            '    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '        <path d="M1 1L5 5L9 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+            '    </svg>\n' +
+            '</button>',
+        nextArrow: '<button class="slide-button slide-button--next">\n' +
+            '    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '        <path d="M1 1L5 5L9 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+            '    </svg>\n' +
+            '</button>'
+    });
 })
