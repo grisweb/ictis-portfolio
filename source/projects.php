0d41073cb7373f559e7acc1e11ebd6af4fd03aae
grisweb <grishakvlad@gmail.com>
2021-03-02 15:38:08 +0300

    02.03.2021v2

--- source/projects.php
+++ source/projects.php
@@ -55,7 +55,7 @@
                         <header class="sidebar__header">
                             <h3 class="sidebar__title">Категория</h3>
                             <button class="sidebar__toggle">
-                                <svg width="16" height="9" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M1 1L5 5L9 1" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                 </svg>
                             </button>
