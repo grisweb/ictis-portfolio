c426b6c337f5a8994b7014347d17b70705977416
Gris12345 <63634935+Gris12345@users.noreply.github.com>
2021-03-12 22:22:17 +0300

    12.03.2021

--- source/NekMarket.php
+++ source/NekMarket.php
@@ -31,12 +31,34 @@
                                    <p class="project-cart__title">Тип проекта:</p>
                                    <p class="project-cart__links"><a href="">Веб-сайт</a>, <a href="">интернет-магазин</a></p>
                                </div>
+                                <div class="project-cart__type-project">
+                                    <p class="project-cart__title">Команда:</p>
+                                    <p class="project-cart__links"><a href="">NekWeb</a></p>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="project-wrapper__project-tabs">
                         <div class="project-tabs">
-
+                            <div class="tabs-triggers">
+                                <a href="#tab-1" class="tabs-triggers__item tabs-triggers__item--active">Описание</a>
+                                <a href="#tab-2" class="tabs-triggers__item">Галерея</a>
+                                <a href="#tab-3" class="tabs-triggers__item">Разработчики</a>
+                                <a href="#tab-4" class="tabs-triggers__item">Тех. описание</a>
+                            </div>
+                            <div class="tabs-content">
+                                <div id="tab-1" class="tabs-content__item tabs-content__item--active">
+                                    NekMarket - это функционриующая платформа интернет-торговли.
+                                    Сайт предоставляет лёгкий доступ продавцам для начала работы и прямого осуществления продаж. Достаточно
+                                    будет лишь зарегистрироваться и заполнить основные данные. При этом для продавцов будут доступен удобный
+                                    личный кабинет, в котором можно подготавливать информацию о новом товаре и в любой удобный момент публиковать
+                                    новое предложение на платформе. Также есть возможность отслеживать статистику продаж, производить сравнение
+                                    аналогов и использовать другие удобные функции.
+                                </div>
+                                <div id="tab-2" class="tabs-content__item"></div>
+                                <div id="tab-3" class="tabs-content__item"></div>
+                                <div id="tab-4" class="tabs-content__item"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -47,6 +69,7 @@
 <?php include('../source/blocks/page-footer.php') ?>
 <script src="../scripts/jquery-3.5.1.min.js"></script>
 <script src="../scripts/page-header.js"></script>
+<script src="../scripts/project.js"></script>
 </body>
 </html>
 
